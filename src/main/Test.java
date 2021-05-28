aee7ca51eeea930385aefb4370c415ee5873ab59
Ubuntu <sharmila.patil@mindtree.com>
2021-05-28 16:15:38 +0000

    third commit

--- src/main/Test.java
+++ src/main/Test.java
@@ -3,7 +3,9 @@ package com.javaproject.DocketJenkinsIntegrate;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		for(int i=0; i<3; i++) {
+			System.out.println("Hello World ! " + i);
+		}
 	}
 
 }
